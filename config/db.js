534711e2a053cea64796bd25b8e494e1443b898d
Shourya-Sinha <shouryasinha.c@gmail.com>
2026-05-22 10:30:55 +0530

    MODIFY MONGODB CONNECTIONS

--- config/db.js
+++ config/db.js
@@ -1,17 +1,49 @@
+// import mongoose from "mongoose";
+// import { CERROR, CLOG } from "../utils/logger.js";
+
+// export default async function connectDB() {
+//   try {
+//     await mongoose.connect(process.env.MONGO_LOCAL_URI);
+
+//     CLOG("MongoDB Connected");
+
+//     // IMPORTANT: return an object
+//     return { isConnected: true };
+//   } catch (err) {
+//      CERROR("DB Error:", err);
+
+//     return { isConnected: false };
+//   }
+// }
 import mongoose from "mongoose";
-import { CERROR, CLOG } from "../utils/logger.js";
+import { CERROR, CLOG } from "../utils/logger.JS";
 
-export default async function connectDB() {
+let isConnected = false;
+
+export async function connectDB() {
+  if (isConnected) {
+    return;
+  }
+
+  if (!process.env.MONGO_URI) {
+    CERROR("❌ Missing MONGO_URI");
+    return;
+  }
+
   try {
-    await mongoose.connect(process.env.MONGO_LOCAL_URI);
+    const conn = await mongoose.connect(process.env.MONGO_URI, {
+      bufferCommands: false,
+      serverSelectionTimeoutMS: 8000,
+      socketTimeoutMS: 45000,
+      maxPoolSize: 5,
+      keepAlive: true,
+    });
 
-    CLOG("MongoDB Connected");
+    isConnected = conn.connections[0].readyState === 1;
 
-    // IMPORTANT: return an object
+    CLOG("✅ MongoDB Connected");
     return { isConnected: true };
   } catch (err) {
-     CERROR("DB Error:", err);
-
-    return { isConnected: false };
+    CERROR("❌ MongoDB Connection Error:", err);
   }
 }
