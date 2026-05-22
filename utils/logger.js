fde327ce855975c82243a8a600c11e2c9d22d1d2
Shourya-Sinha <shouryasinha.c@gmail.com>
2026-05-22 10:22:30 +0530

    use multiple environment logging system

--- utils/logger.js
+++ utils/logger.js
@@ -1,25 +1,25 @@
-import fs from "fs";
+// import fs from "fs";
 
-const logFile = "logs.txt";
+// const logFile = "logs.txt";
 
-function writeLog(type, args) {
-  const log = `[${new Date().toISOString()}] [${type}] ${args.join(" ")}\n`;
-  fs.appendFileSync(logFile, log);
-}
+// function writeLog(type, args) {
+//   const log = `[${new Date().toISOString()}] [${type}] ${args.join(" ")}\n`;
+//   fs.appendFileSync(logFile, log);
+// }
 
 export function CLOG(...args) {
   console.log("\x1b[32m%s\x1b[0m", "[LOG]", ...args);
-  writeLog("LOG", args);
+  // writeLog("LOG", args);
 }
 
 export function CWARNING(...args) {
   console.warn("\x1b[33m%s\x1b[0m", "[WARN]", ...args);
-  writeLog("WARN", args);
+  // writeLog("WARN", args);
 }
 
 export function CERROR(...args) {
   console.error("\x1b[31m%s\x1b[0m", "[ERROR]", ...args);
-  writeLog("ERROR", args);
+  // writeLog("ERROR", args);
 }
 
 // export function CLOG(...args) {
